3d9f2deeaf263183c67caac8bd96c5638de69cd1
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-08-14 15:57:27 -0300

    Create 2137.cpp

---
+++ 2137.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <iostream>
+#include <stdio.h>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+
+	while(cin >> n)
+	{
+		vector<string> codigos;
+		string code;
+
+		while(n--)
+		{
+			cin >> code;
+			codigos.push_back(code);
+		}
+
+		sort(codigos.begin(), codigos.end());
+
+		for(int i = 0; i < codigos.size(); ++i)
+			cout << codigos[i] << '\n';
+
+		codigos.clear();
+	}
+
+	return 0;
+}
