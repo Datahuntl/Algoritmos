8c3afc7a0f246caea443291a2c2bb97edfeb0f3f
Datahuntl <103469153+Datahuntl@users.noreply.github.com>
2024-08-14 15:56:24 -0300

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	int Numero1; // Numero Inteiro "A"
+	int Numero2; //Numero Inteiro "B"
+	int Soma; //Soma dos dois valores "A" e "B"
+	
+	cin >> Numero1; //Entrada de "A"
+	cin >> Numero2; //Entrada de "B"
+	
+	Soma = Numero1 + Numero2; //Armazenando a soma de "A" + "B"
+	
+	cout << "X = " << Soma << endl;
+	
+	return 0;
+}
